df7ba6fe16d962379c35295326148fdccba091b0
flored27 <dflores@fandm.edu>
2017-10-15 21:46:13 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+Insert Into bears (name, age, gender, color, temperament, alive) Values ("Mr.Chocolate", 4, "M", "Brown", "Gentle", 1);
+Insert Into bears (name, age, gender, color, temperament, alive) Values ("Rowdy", 3, "M", "Brown", "Gentle", 1);
+Insert Into bears (name, age, gender, color, temperament, alive) Values ("Tabitha", 2, "F", "Black", "Gentle", 1);
+Insert Into bears (name, age, gender, color, temperament, alive) Values ("Sergeant Brown", 1, "M", "Black", "Gentle", 1);
+Insert Into bears (name, age, gender, color, temperament, alive) Values ("Melissa", 2, "F", "Brown", "Gentle", 1);
+Insert Into bears (name, age, gender, color, temperament, alive) Values ("Grinch", 4, "M", "White", "Gentle", 1);
+Insert Into bears (name, age, gender, color, temperament, alive) Values ("Wendy", 1, "F", "Brown", "Gentle", 1);
+Insert Into bears (name, age, gender, color, temperament, alive) Values (null, 5, "M", "Brown", "Gentle", 0);
